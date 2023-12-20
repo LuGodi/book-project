8952ab30150e8ebb687c857ffee296f01ffa2de1
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2023-12-20 10:50:51 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "read" : "not read yet"
+    }`;
+  };
+}
+
+const book1 = new Book("Midnight Library", "Matt Huang", "409", true);
+
+console.log(book1);
+console.log(book1.info());
