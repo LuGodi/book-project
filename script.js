4e202a33f545e0c05cbfdd50641839683121a2ab
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2024-01-19 15:09:15 -0300

    Add book titles and buttons

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const myLibrary = [];
+const library = document.querySelector(".library");
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -11,6 +14,32 @@ function Book(title, author, pages, read) {
 }
 
 const book1 = new Book("Midnight Library", "Matt Huang", "409", true);
+const book2 = new Book("Harry Potter", "Jk Rowling", "200", false);
+const book3 = new Book("Im tired", "myself", "1", true);
 
 console.log(book1);
 console.log(book1.info());
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+function displayBooks() {
+  const myDivs = [];
+  for (let book of myLibrary) {
+    console.log(book);
+    const bookElement = document.createElement("div");
+    const nameParagraph = document.createElement("p");
+
+    nameParagraph.textContent = book.title;
+    bookElement.appendChild(nameParagraph);
+    myDivs.push(bookElement);
+  }
+  library.append(...myDivs);
+}
+displayBooks();
+// qual a diferença do return de Object.getPrototypeOf(Player) pra Player.prototype ? toda
+//Player.prototype eh uma propriedade da funcao, todas as instancias criadas atraves do constructor player using new vao ter uma referencia a Player.prototype
+//Object.getPrototypeOf(Player) eh a hidden property da funcao Player. Eh codigo nativo, que tem um prototype que referencia Object.prototype
